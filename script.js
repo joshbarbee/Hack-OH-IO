b3b1d460d4e84060bfe6d4d1c4189aef4c99a17a
cscheatzle20 <carterscheatzle@gmail.com>
2021-11-06 13:47:57 -0400

    front-end setup

---
+++ script.js
@@ -0,0 +1,6 @@
+<script>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+	<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"></link>
+
+    
+</script>
