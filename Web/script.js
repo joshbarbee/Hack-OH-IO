c72c5056e2855b03c5bab86f4f0301bf3cb232f7
Matt LeHoty <mattlehoty@gmail.com>
2021-11-07 00:25:09 -0400

    coommmit

--- Web/script.js
+++
@@ -1,8 +0,0 @@
-<script>
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
-	<link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet"></link>
-
-    
-
-    
-</script>
